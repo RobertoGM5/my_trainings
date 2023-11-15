2085167edde5d6621561a50886ba92cde07bf816
Shilpajordan <Shilpa.nair@dci-student.org>
2023-11-15 16:06:13 +0100

    exception exercises

--- object_oriented/try_except/try_except.py
+++ object_oriented/try_except/try_except.py
@@ -6,3 +6,40 @@ while True:
         break
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
+
+
+## 1.
+a=5
+b=0
+try:
+    result=a/b
+    
+except ZeroDivisionError:
+    result ="You can't divide by 0"
+print(result)
+# 2.
+
+#Type your answer below.
+
+a="Hello World!"
+try:
+    a + 10
+except BaseException as e:
+    msg="You can't add int to string"
+
+
+print(msg)
+
+# 3.
+
+#Type your answer below.
+
+lst=[5, 10, 20]
+
+try:
+    print(lst[5])
+except IndexError:
+    msg="You're out of list range"
+
+
+print(msg)
